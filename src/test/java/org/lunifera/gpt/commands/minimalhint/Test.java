fe27e7c8e477e6044697b3e5eacd239bb1396e97
Florian Pirchner <florian.pirchner@gmail.com>
2023-04-13 17:14:44 +0200

    Did some cleanup.

--- src/test/java/org/lunifera/gpt/commands/minimalhint/Test.java
+++ src/test/java/org/lunifera/gpt/commands/minimalhint/Test.java
@@ -17,25 +17,31 @@ public class Test {
 		CommandApi api = new CommandApi(OPENAI_KEY);
 		api.setCommands(this::createCommands);
 
+		// Ask your boss
 		ICommand command = api.queryCommand("Need more money");
 		assertEquals(AskYourBossCommand.NAME, command.getName());
 		
+		// Do web query
 		command = api.queryCommand("Info about quantum physics.");
 		assertEquals(WebSearchCommand.NAME, command.getName());
 		
+		// As friends
 		command = api.queryCommand("I need some one to talk.");
 		assertEquals(AskYourFriendsCommand.NAME, command.getName());
-
-		command = api.queryCommand("I need some info about quantum physics.");
-		assertEquals(WebSearchCommand.NAME, command.getName());
 		
+		// Also ask friends
 		command = api.queryCommand("My friend is physicist. I need some info about quantum physics.");
 		assertEquals(AskYourFriendsCommand.NAME, command.getName());
 		
+		// Ask you doctor
 		command = api.queryCommand("My head feels hot");
 		assertEquals(AskYourDoctorCommand.NAME, command.getName());
 	}
 
+	/**
+	 * Create the list of commands.
+	 * @return
+	 */
 	private List<ICommand> createCommands() {
 		List<ICommand> commands = new ArrayList<>();
 		commands.add(new AskYourBossCommand());
